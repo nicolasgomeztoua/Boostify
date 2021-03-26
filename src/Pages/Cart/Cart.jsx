c69535feb7d7a9e8523fc4605eaf05cc616e43b5
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-03-26 12:00:15 +0100

    stripe

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -19,6 +19,29 @@ const Cart = () => {
     dispatch({ type: "REMOVE", index });
   };
 
+  const handleClick = async (event) => {
+    // Get Stripe.js instance
+    const stripe = await stripePromise;
+
+    // Call your backend to create the Checkout Session
+    const response = await fetch("/create-checkout-session", {
+      method: "POST",
+    });
+
+    const session = await response.json();
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
+
   return (
     <>
       <Navbar></Navbar>
@@ -73,7 +96,9 @@ const Cart = () => {
             {" "}
             $ {totalPrice}
           </span>
-          <button role="link">Checkout</button>
+          <button role="link" onClick={handleClick}>
+            Checkout
+          </button>
         </div>
       </div>
     </>
