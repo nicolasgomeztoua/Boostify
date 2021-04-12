dbd49e5a6820e5bafe3c56826dd6ea6da93d11e9
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-12 16:01:55 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./Pages/Home/Home";
 
-import KillBoost from "./Pages/KillBoost";
 //routing
 import PrivateRoute from "./Pages/authComponents/PrivateRoute";
 ///////////////////////////////////////////////////////////////
 import ContactUs from "./Pages/ContactUs/ContactUs";
 import RankBoost from "./Pages/RankBoost/RankBoost";
 import AcheivementBadges from "./Pages/AcheivementBadges/AcheivementBadges.jsx";
-import WinBoost from "./Pages/WinBoost";
+import Home from "./Pages/Home/Home";
 import Login from "./Pages/authComponents/Login";
 import Register from "./Pages/authComponents/Register";
 import ForgotPassword from "./Pages/authComponents/ForgotPassword";
@@ -26,7 +24,7 @@ function App() {
         <div className="app">
           <Switch>
             <PrivateRoute exact path="/cart" component={Cart}></PrivateRoute>
-            <Route exact path="/" component={Home}></Route>
+            <Route path="/" exact component={Home}></Route>
             <Route path="/rank-boosting" exact component={RankBoost}></Route>
             <Route path="/contact-us" exact component={ContactUs}></Route>
             <Route path="/kills-boosting" exact component={ComingSoon}></Route>
