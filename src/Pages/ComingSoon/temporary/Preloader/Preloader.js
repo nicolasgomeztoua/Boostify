6066fc0e8916b60d9b14cedc9060a52aaaa3ea27
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-12 16:48:57 +0200

    home

--- src/Pages/ComingSoon/temporary/Preloader/Preloader.js
+++ src/Pages/ComingSoon/temporary/Preloader/Preloader.js
@@ -8,8 +8,8 @@ class Preloader extends Component {
       preload.style.opacity = "0";
       setTimeout(() => {
         preload.style.display = "none";
-      }, 1000);
-    }, 3000);
+      }, 50);
+    }, 200);
   }
 
   componentDidMount() {
