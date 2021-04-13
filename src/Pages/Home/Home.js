9a4b84afbd0b058ddb2e4ade52317d65cc720520
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-13 02:34:37 +0200

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -63,10 +63,10 @@ const Home = () => {
   return (
     <div>
       <Helmet>
-        <title>Boostify | Cheap Apex Legends Boosting Services</title>
+        <title>Boostify | Cheap Apex Legends Boosting Services Playstation</title>
         <meta
           name="description"
-          content="Get boosted by our professionals for a cheap price and achieve higher Ranks in Apex Legends. Our professionals consist of only All-seasons Apex predators. Veterans. 24/7 Live Chat Support. Cheap. Get boosted by the best. Same Day Deliver Ranked boost."
+          content="Get boosted by our professionals for a cheap price and achieve higher Ranks in Apex Legends. Our professionals consist of only All-seasons Apex predators. Veterans. 24/7 Live Chat Support. Cheap. Get boosted by the best. Same Day Deliver Ranked boost and Badge boost."
         />
         <meta name="theme-color" content="#008f68" />
       </Helmet>
