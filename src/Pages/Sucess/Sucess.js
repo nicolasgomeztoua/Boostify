9f6efdb7bce705b45ef979edc6a0a8132cbfcd64
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-20 23:31:03 +0200

    success

--- src/Pages/Sucess/Sucess.js
+++ src/Pages/Sucess/Sucess.js
@@ -12,6 +12,7 @@ const Sucess = () => {
   const clearCart = () => {
     localStorage.removeItem("cart");
   };
+
   window.gtag("event", "purchase", {
     send_to: "AW-388171063/qII5CLeDi4gCELeKjLkB",
     value: totalPrice,
