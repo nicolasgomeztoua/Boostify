93c3832a1aa0134413e6eb8f6be6bdb9790e9cef
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-07-12 21:50:25 +0200

    Update stats.js

--- src/Pages/Home/stats.js
+++ src/Pages/Home/stats.js
@@ -6,8 +6,8 @@ import { CalendarExclamation } from "@styled-icons/boxicons-regular/CalendarExcl
 import styled from "styled-components";
 
 export const walzyEditThis = {
-  ordersCompleted: 1227,
-  ordersQueued: 7,
+  ordersCompleted: 1242,
+  ordersQueued: 10,
 };
 
 const Stats = () => {
