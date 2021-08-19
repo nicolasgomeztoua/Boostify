37763925df3e605f59a27acdf3c44c0eb68a2f3e
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-08-19 21:05:12 +0200

    gg

--- src/Pages/SpecialSignUp/SignUp.jsx
+++ src/Pages/SpecialSignUp/SignUp.jsx
@@ -1,63 +1,71 @@
-import styled from 'styled-components'
-import mobileBackg from "../Images/SignUpMobile.jpg"
-import desktopBackg from "../Images/SignUpPc.jpg"
-import SpecialRegister from './SpecialRegister'
-import { useState } from 'react'
+import styled from "styled-components";
+import mobileBackg from "../Images/SignUpMobile.jpg";
+import desktopBackg from "../Images/SignUpPc.png";
+import SpecialRegister from "./SpecialRegister";
+import { useState } from "react";
 const SignUp = ({ history }) => {
-    const [displayForm, setDisplayForm] = useState('none')
-    const [displayButton, setDisplayButton] = useState('block')
+  const [displayForm, setDisplayForm] = useState("none");
+  const [displayButton, setDisplayButton] = useState("block");
+  const [grid, setGrid] = useState("grid");
 
-
-    const Container = styled.div`
-@media(orientation: landscape){background-image:url(${desktopBackg}); display:grid;
-grid-template-columns: 1fr 1fr;}
-@media(orientation: portrait){background-image:url(${mobileBackg});}
-height:100vh;
-background-repeat: no-repeat;
-background-size: cover;
-color:white;
-
-`
-    const Padder = styled.div`
-@media(orientation: landscape){width:40vw;}
-@media(orientation: portrait) {padding-top: 40vh;}
-`
-    const ContentGrid = styled.section`
-display:flex;
-flex-direction:column;
-`
-    const Title = styled.header`
-font-size:1.55rem;
-color: #fff;
+  const Container = styled.div`
+    @media (orientation: landscape) {
+      background-image: url(${desktopBackg});
+      display: ${grid};
+      grid-template-columns: 1fr 1fr;
+    }
+    @media (orientation: portrait) {
+      background-image: url(${mobileBackg});
+    }
+    height: 100vh;
+    background-repeat: no-repeat;
+    background-size: cover;
+    color: white;
+  `;
+  const Padder = styled.div`
+    @media (orientation: landscape) {
+      width: 40vw;
+    }
+    @media (orientation: portrait) {
+      padding-top: 40vh;
+    }
+  `;
+  const ContentGrid = styled.section`
+    display: flex;
+    flex-direction: column;
+  `;
+  const Title = styled.header`
+    font-size: 1.55rem;
+    color: #fff;
     font-weight: 700;
     line-height: 1em;
     padding: 28px 10vw 0;
     text-align: center;
-    text-shadow: 0.07em 0 black, 0 0.07em black, -0.07em 0 black, 0 -0.07em black,
-    -0.07em -0.07em black, -0.07em 0.07em black, 0.07em -0.07em black,
-    0.07em 0.07em black;
-`
-    const Body = styled.h2`
-    font-size: .9rem;
+    text-shadow: 0.07em 0 black, 0 0.07em black, -0.07em 0 black,
+      0 -0.07em black, -0.07em -0.07em black, -0.07em 0.07em black,
+      0.07em -0.07em black, 0.07em 0.07em black;
+  `;
+  const Body = styled.h2`
+    font-size: 0.9rem;
     color: #fff;
     font-weight: 100;
     margin-top: 5px;
     text-align: center;
-    text-shadow: 0.07em 0 black, 0 0.07em black, -0.07em 0 black, 0 -0.07em black,
-    -0.07em -0.07em black, -0.07em 0.07em black, 0.07em -0.07em black,
-    0.07em 0.07em black;
-`
-    const Subtitle = styled.h3`
-    font-size: .9rem;
+    text-shadow: 0.07em 0 black, 0 0.07em black, -0.07em 0 black,
+      0 -0.07em black, -0.07em -0.07em black, -0.07em 0.07em black,
+      0.07em -0.07em black, 0.07em 0.07em black;
+  `;
+  const Subtitle = styled.h3`
+    font-size: 0.9rem;
     color: #fff;
     font-weight: 700;
     text-align: center;
-    text-shadow: 0.07em 0 black, 0 0.07em black, -0.07em 0 black, 0 -0.07em black,
-    -0.07em -0.07em black, -0.07em 0.07em black, 0.07em -0.07em black,
-    0.07em 0.07em black;
-`
-    const Button = styled.button`
-    font-size: .9rem;
+    text-shadow: 0.07em 0 black, 0 0.07em black, -0.07em 0 black,
+      0 -0.07em black, -0.07em -0.07em black, -0.07em 0.07em black,
+      0.07em -0.07em black, 0.07em 0.07em black;
+  `;
+  const Button = styled.button`
+    font-size: 0.9rem;
     background-color: #fff;
     border: solid #056e84 1px;
     border-radius: 25px;
@@ -71,59 +79,64 @@ color: #fff;
     margin-top: 10px;
     min-height: 30px;
     min-width: 74.0740740741%;
-    padding: .25rem .5rem;
+    padding: 0.25rem 0.5rem;
     position: relative;
     text-align: center;
     text-decoration: none;
     text-transform: uppercase;
     vertical-align: middle;
     transition: all 0.3s ease 0s;
-    &&:hover{
-        background-color: #2EE59D;
-  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
-  color: #fff;
-  transform: translateY(-7px);
+    &&:hover {
+      background-color: #2ee59d;
+      box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+      color: #fff;
+      transform: translateY(-7px);
     }
-`
+  `;
 
-    const ButtonContainer = styled.div`
+  const ButtonContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     width: 100%;
-`
+  `;
 
-    const onClickSignUp = () => {
-        setTimeout(() => {
-            setDisplayButton('none')
-            setDisplayForm('flex')
-        }, 400)
+  const onClickSignUp = () => {
+    setTimeout(() => {
+      setDisplayButton("none");
+      setDisplayForm("flex");
+      setGrid("flex");
+    }, 400);
+  };
+  return (
+    <Container>
+      <Padder style={{ display: displayButton }} />
+      <ContentGrid style={{ display: displayButton }}>
+        <div>
+          <Title>Free 4k/20Bomb for NEW Boostify users</Title>
+        </div>
+        <div>
+          <Body>Sign up today!</Body>
+          <Subtitle>
+            AND RECEIVE A FREE 4k/20-BOMB WITH ANY RANK BOOST ORDER!
+          </Subtitle>
+        </div>
+        <div>
+          <ButtonContainer>
+            <Button onClick={onClickSignUp}> Sign Up!</Button>
+          </ButtonContainer>
+          <ButtonContainer>
+            <Button>Cancel</Button>
+          </ButtonContainer>
+        </div>
+      </ContentGrid>
+      <SpecialRegister
+        display={displayForm}
+        history={history}
+      ></SpecialRegister>
+    </Container>
+  );
+};
 
-    }
-    return (
-        <Container>
-            <Padder style={{ display: displayButton }} />
-            <ContentGrid style={{ display: displayButton }}>
-                <div>
-                    <Title>Free 4k/20Bomb for NEW Boostify users</Title>
-                </div>
-                <div>
-                    <Body>Sign up today!</Body>
-                    <Subtitle>AND RECEIVE A FREE 4k/20-BOMB WITH ANY RANK BOOST ORDER!</Subtitle>
-                </div>
-                <div>
-                    <ButtonContainer>
-                        <Button onClick={onClickSignUp}> Sign Up!</Button>
-                    </ButtonContainer>
-                    <ButtonContainer>
-                        <Button>Cancel</Button>
-                    </ButtonContainer>
-                </div>
-            </ContentGrid>
-            <SpecialRegister display={displayForm} history={history}></SpecialRegister>
-        </Container>
-    )
-}
-
-export default SignUp
+export default SignUp;
