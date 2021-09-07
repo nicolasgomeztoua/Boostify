e7afdbd432f7e57689baeffb6e5e5f1c3aaf3fcf
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-09-07 19:39:03 +0200

    editing animation

--- src/Pages/SpecialSignUp/step.jsx
+++ src/Pages/SpecialSignUp/step.jsx
@@ -1,35 +1,36 @@
+import { Button, StepContainer } from "./signupstyles";
+const Step = ({ icon, textfile, containerRef, step }) => {
+  const Icon = icon;
+  const iconStyles = { height: "100px" };
 
-import { Button, StepContainer } from "./signupstyles"
-const Step = ({ icon, textfile, containerRef }) => {
-   const Icon = icon
-   const iconStyles = { height: "100px" }
+  function scrollToRight() {
+    containerRef.current.scrollTo({
+      left: step * window.innerWidth,
+      behavior: "smooth",
+    });
+    console.log(containerRef.current);
+  }
 
-   function scrollToRight() {
+  return (
+    <StepContainer>
+      <div class="services">
+        <div class="content content-1">
+          <Icon style={iconStyles}></Icon>
+          <h2>{textfile.title}</h2>
+          {textfile.time === true ? "" : ""}
+          <p>{textfile.body}</p>
+          <Button
+            background={"black"}
+            color={"white"}
+            border={"none"}
+            onClick={scrollToRight}
+          >
+            {textfile.button}
+          </Button>
+        </div>
+      </div>
+    </StepContainer>
+  );
+};
 
-      containerRef.current.scrollLeft += window.innerWidth;
-      console.log(containerRef.current)
-      }
-     
-   return (
-
-      <StepContainer>
-
-         <div class="services">
-            <div class="content content-1">
-               <Icon style={iconStyles}></Icon>
-               <h2>
-                  {textfile.title}
-               </h2>
-               {textfile.time === true ? '' : ''}
-               <p>
-                  {textfile.body}
-               </p>
-               <Button background={'black'} color={'white'} border={"none"} onClick={scrollToRight}>{textfile.button}</Button>
-            </div>
-         </div>
-      </StepContainer>
-
-   )
-}
-
-export default Step
+export default Step;
--- src/Pages/SpecialSignUp/typ.jsx
+++ src/Pages/SpecialSignUp/typ.jsx
@@ -1,28 +1,45 @@
-
-import Step from "./step"
-import { ThankYou, firstStep, secondStep, finalStep } from "./texts/textfile"
-import {CheckmarkCircle} from "@styled-icons/evaicons-solid/CheckmarkCircle"
-import Navbar from "../../Navbar/Navbar"
-import { TypContainer } from "./signupstyles"
-import { useRef } from "react"
-import {Offer} from "@styled-icons/boxicons-solid/Offer"
-import {Timer} from "@styled-icons/boxicons-regular/Timer"
-
+import Step from "./step";
+import { ThankYou, firstStep, secondStep, finalStep } from "./texts/textfile";
+import { CheckmarkCircle } from "@styled-icons/evaicons-solid/CheckmarkCircle";
+import Navbar from "../../Navbar/Navbar";
+import { TypContainer } from "./signupstyles";
+import { useRef } from "react";
+import { Offer } from "@styled-icons/boxicons-solid/Offer";
+import { Timer } from "@styled-icons/boxicons-regular/Timer";
 
 const Typ = () => {
-  const containerRef = useRef(TypContainer)
-    return (      
-      <>
-    <Navbar></Navbar>
-      <TypContainer ref={containerRef}> 
-        <Step textfile={ThankYou} containerRef={containerRef} icon={CheckmarkCircle}></Step>
-        <Step textfile={firstStep} containerRef={containerRef} icon={Offer}></Step>
-        <Step textfile={secondStep} containerRef={containerRef} icon={Offer}></Step>
-        <Step textfile={finalStep} containerRef={containerRef} icon={Timer}></Step>
+  const containerRef = useRef(TypContainer);
+  return (
+    <>
+      <Navbar></Navbar>
+      <TypContainer ref={containerRef}>
+        <Step
+          textfile={ThankYou}
+          containerRef={containerRef}
+          icon={CheckmarkCircle}
+          step={1}
+        ></Step>
+        <Step
+          textfile={firstStep}
+          containerRef={containerRef}
+          icon={Offer}
+          step={2}
+        ></Step>
+        <Step
+          textfile={secondStep}
+          containerRef={containerRef}
+          icon={Offer}
+          step={3}
+        ></Step>
+        <Step
+          textfile={finalStep}
+          containerRef={containerRef}
+          icon={Timer}
+          step={4}
+        ></Step>
       </TypContainer>
-  
-      </>
-    )
-}
+    </>
+  );
+};
 
-export default Typ
+export default Typ;
