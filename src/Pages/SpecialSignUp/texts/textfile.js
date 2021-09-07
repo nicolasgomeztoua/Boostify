7670c138f25f5e7cc5ce0f0e2ee1fa2bf05a6d11
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-09-07 19:00:43 +0200

    Update textfile.js

--- src/Pages/SpecialSignUp/texts/textfile.js
+++ src/Pages/SpecialSignUp/texts/textfile.js
@@ -4,18 +4,18 @@ body: 'Through our Special Sign Up, You Qualify For A Set Of Offers Available Fo
 button:'Show Me!'
 }
 export const firstStep ={
-title: 'Thank you for registering!',
-body: 'Through our Special Sign Up, You Qualify For A Set Of Offers Available For A Short Time, Take Advantage Of Them!',
-button:'Show Me!'
+title: 'Our first offer is for a free 4k Damage Badge!',
+body: 'Purchase $100+ worth of rank boost and get a free 4K Badge on ANY legend of your choice!',
+button:'Tell Me More!'
 }
 
 export const secondStep ={
-    title: 'Thank you for registering!',
-    body: 'Through our Special Sign Up, You Qualify For A Set Of Offers Available For A Short Time, Take Advantage Of Them!',
-    button:'Show Me!'
+    title: 'Our second offer is for a free 4k Damage Badge and 20 bomb badge!',
+    body: 'To be eligible for this great offer, purchase $200+ worth of rank boost and get your free badges on ANY legend of your choice!',
+    button:'Tell me more!'
 }
 export const finalStep ={
     title: 'This offer Expires in:',
-    body: 'Go to our rank boost page and save on your favourite badges.',
+    body: 'By clicking on the button below, you will be directed to our rank boost page where your could save up to $100 in ypur favorite badges!',
     button:'Take Me There!'
 }
