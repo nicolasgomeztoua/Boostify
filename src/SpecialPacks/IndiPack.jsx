3fc4102d2cc8a8ce6f0922032f867aa9fa726f5a
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-12-26 18:30:19 +0100

    gg

--- src/SpecialPacks/IndiPack.jsx
+++ src/SpecialPacks/IndiPack.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import styled from "styled-components";
-import Plat from "../Pages/Images/Ranked_Tier4_Platinum.png"
-import Masters from "../Pages/Images/Ranked_Tier6_Master.png"
-import PackDesc from "./PackDesc"
+import Plat from "../Pages/Images/Ranked_Tier4_Platinum.png";
+import Masters from "../Pages/Images/Ranked_Tier6_Master.png";
+import PackDesc from "./PackDesc";
 
 const IndiPack = ({ title }) => {
+  const [main, setMain] = useState('')
   const SpecialPackTitle = styled.h1`
     font-size: calc(3vw + 20px);
     background: rgb(151, 228, 226);
-    padding:5px;
+    padding: 5px;
     background: ${(props) =>
       `linear-gradient(90deg, #${props.firstColor} 0%, #${props.secondColor} 80%)`};
+    background-clip: text;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     border-bottom: 3px solid #97e4e2;
@@ -23,19 +26,20 @@ const IndiPack = ({ title }) => {
 
   const SelectionGrid = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr ;
-    grid-template-rows:1fr;
-    flex-direction:row;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    flex-direction: row;
   `;
+  console.log(main)
   return (
     <ProductWrapper>
       <SpecialPackTitle firstColor="4de6e0" secondColor="9c64ca">
         {title}
       </SpecialPackTitle>
       <SelectionGrid>
-        <PackDesc initialImg={Plat} finalImg={Masters}/>
-        <PackDesc initialImg={Plat} finalImg={Masters}/>
-        <PackDesc initialImg={Plat} finalImg={Masters}/>
+        <PackDesc initialImg={Plat} finalImg={Masters} setMain={setMain}/>
+        <PackDesc initialImg={Plat} finalImg={Masters} />
+        <PackDesc initialImg={Plat} finalImg={Masters} />
       </SelectionGrid>
     </ProductWrapper>
   );
